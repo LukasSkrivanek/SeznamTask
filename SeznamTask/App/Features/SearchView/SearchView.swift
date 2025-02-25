19b2e61c616aa89c395ae1bc907340732f03a9f1
LukasSkrivanek <luky.skrivos@gmail.com>
2025-02-25 13:41:28 +0100

    [Initial] Settings project a basic structure
    Add SwiftLint script
    Add SwiftFormat script
    Add Swinject  Dependency
    Create  Structure for project

--- SeznamTask/App/Features/SearchView/SearchView.swift
+++ SeznamTask/App/Features/SearchView/SearchView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  SearchView.swift
 //  SeznamTask
 //
 //  Created by macbook on 25.02.2025.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SearchView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    SearchView()
 }
