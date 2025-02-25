e10b5bb9212c21e195eea9d747a54b3eab16baa9
LukasSkrivanek <luky.skrivos@gmail.com>
2025-02-25 12:27:18 +0100

    Initial Commit

---
+++ SeznamTask/SeznamTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeznamTaskApp.swift
+//  SeznamTask
+//
+//  Created by macbook on 25.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SeznamTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
